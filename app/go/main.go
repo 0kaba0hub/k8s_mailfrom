bb88f6471250c1be3ee7f92d7a4cbea4a49de2b4
Ihor Rusyn <0kaba0@gmail.com>
2026-05-02 18:16:40 +0200

    chore(go): add copyright header to main.go

--- app/go/main.go
+++ app/go/main.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2026 The mailfrom-milter Authors. All rights reserved.
+// Use of this source code is governed by a GNU GPLv3 style
+// license that can be found in the LICENSE file.
+//
 // mailfrom-milter — Postfix milter that enforces From-header / envelope-from
 // domain alignment for authenticated SMTP sessions.
 //
